9161ed436273e112524de7dcce726e6b8edf4247
Carl Eiserman <carleiserman@gmail.com>
2017-10-13 15:40:33 +0200

    feature: added logout in settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 import {OnInit} from "@angular/core";
 import {ErrorsProvider} from "../../providers/errors/errors";
+import {AlertController} from 'ionic-angular';
+import {LandingPage} from "../landing/landing";
 
 /**
  * Generated class for the SettingsPage page.
@@ -24,7 +26,8 @@ export class SettingsPage {
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 public platform: Platform,
-                public errors: ErrorsProvider) {
+                public errors: ErrorsProvider,
+                public alertCtrl: AlertController) {
     }
 
     ngOnInit() {
@@ -49,6 +52,49 @@ export class SettingsPage {
 
     ionViewDidEnter() {
 
+    }
+
+    presentLogoutModal(): void {
+        let confirm = this.alertCtrl.create({
+            title: 'Logout?',
+            message: 'Are you sure you want to log out? This will clear your saved username and password, and you ' +
+            'will have to type it in again next time you log in.',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: () => {
+                        console.log('Disagree clicked');
+                    }
+                },
+                {
+                    text: 'Logout',
+                    handler: () => {
+                        console.log('Agree clicked');
+                        this.logout();
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
+
+    logout(): void {
+        let rootNav = this.getRootNav(this.navCtrl);
+        rootNav.setRoot(LandingPage, {}, {animate: true, direction: 'backward'});
+    }
+
+    /**
+     * Get the very first navcontroller
+     * to navigate away from the tabs page
+     * @param {NavController} nav
+     * @returns {NavController}
+     */
+    getRootNav(nav: NavController): NavController {
+        let root = nav;
+        while (root.parent != null) {
+            root = root.parent;
+        }
+        return root;
     }
 
 }
