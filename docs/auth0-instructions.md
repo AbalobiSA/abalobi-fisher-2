a4fb67be790bead130915fbfe65f58301966a56c
Carl Eiserman <carleiserman@gmail.com>
2017-11-07 12:37:18 +0200

    chore: added more docs & version bump script

---
+++ docs/auth0-instructions.md
@@ -0,0 +1,10 @@
+# Setting up Auth0
+
+Go to the Application Settings section in your 
+Auth0 dashboard and set your Callback URL in 
+the Allowed Callback URLs box.
+
+```
+# replace YOUR_PACKAGE_ID with your app package ID
+YOUR_PACKAGE_ID://abalobi.eu.auth0.com/cordova/YOUR_PACKAGE_ID/callback
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
 let gulp = require("gulp");
 const bump = require('gulp-bump');
+const fs = require('fs');
+const xml = require('xml2js');
 
 
 gulp.task("default", () => {
-    return gulp.src('./package.json')
-        .pipe(bump())
-        .pipe(gulp.dest("./"));
+    increasePackageVersion()
+        .then(increaseAppVersion)
+        .then(done => console.log("done!"))
+        .catch(ex => {
+            console.log(ex);
+        })
 });
+
+const increasePackageVersion = () => {
+    return new Promise((resolve, reject) => {
+        gulp.src('./package.json')
+            .pipe(bump())
+            .pipe(gulp.dest("./"))
+            .on('end', resolve)
+
+    });
+};
+
+const increaseAppVersion = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./config.xml", (err, file) => {
+            const configFile = file.toString();
+            xml.parseString(configFile, (err, result) => {
+
+                let currentVersion = require("./package.json").version;
+                let currentXML = result;
+                currentXML.widget.$.version = currentVersion;
+
+                const builder = new xml.Builder();
+                const final = builder.buildObject(currentXML);
+
+                fs.writeFile("./config.xml", final, 'utf-8', (error) => {
+                    console.log("file written!");
+                    resolve();
+                });
+
+            });
+        });
+    })
+};
