86dde19cb435ae36eb208b919d72d1ed3a63cd68
Carl Eiserman <carleiserman@gmail.com>
2017-12-08 12:12:48 +0200

    release: v2.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,53 @@ const xml = require('xml2js');
 
 
 gulp.task("default", () => {
-    increasePackageVersion()
-        .then(increaseAppVersion)
-        .then(done => console.log("done!"))
-        .catch(ex => {
-            console.log(ex);
-        })
+  increasePackageVersion()
+    .then(increaseAppVersion)
+    .then(done => console.log("done!"))
+    .catch(ex => {
+      console.log(ex);
+    })
 });
 
 const increasePackageVersion = () => {
-    return new Promise((resolve, reject) => {
-        gulp.src('./package.json')
-            .pipe(bump())
-            .pipe(gulp.dest("./"))
-            .on('end', resolve)
+  return new Promise((resolve, reject) => {
+    gulp.src('./package.json')
+      .pipe(bump())
+      .pipe(gulp.dest("./"))
+      .on('end', resolve)
 
-    });
+  });
 };
 
 const increaseAppVersion = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile("./config.xml", (err, file) => {
-            const configFile = file.toString();
-            xml.parseString(configFile, (err, result) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./config.xml", (err, file) => {
+      const configFile = file.toString();
+      xml.parseString(configFile, (err, result) => {
 
-                let currentVersion = require("./package.json").version;
-                let currentXML = result;
-                currentXML.widget.$.version = currentVersion;
+        const currentVersion = require("./package.json").version;
+        let currentXML = result;
 
-                const builder = new xml.Builder();
-                const final = builder.buildObject(currentXML);
+        currentXML.widget.$.version = currentVersion;
+        currentXML.widget.$['android-versionCode'] = getAndroidVersion(currentVersion);
 
-                fs.writeFile("./config.xml", final, 'utf-8', (error) => {
-                    console.log("file written!");
-                    resolve();
-                });
+        const builder = new xml.Builder();
+        const final = builder.buildObject(currentXML);
 
-            });
+        fs.writeFile("./config.xml", final, 'utf-8', (error) => {
+          console.log("file written!");
+          resolve();
         });
-    })
+
+      });
+    });
+  })
+};
+
+const getAndroidVersion = (input) => {
+  const nums = input.split('.');
+  const major = nums[0] * 100000;
+  const minor = nums[1] * 1000;
+  const patch = nums[2] * 10;
+  return major + minor + patch;
 };
